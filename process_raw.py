888eb99ee17b8d260b25281b6d83c22712d18578
Steven Porter <snowballsteve3@gmail.com>
2015-11-19 21:16:58 -0500

    Python script to convert to json, and or process data

--- process_raw.py
+++ process_raw.py
@@ -1,1 +1,19 @@
-__author__ = 'steve'
+__author__ = 'Steven Porter'
+import pandas as pd
+import os
+
+class ProcessError(Exception):
+    pass
+
+if __name__=="__main__":
+    for file in os.listdir('raw_data'):
+        base,ext = os.path.splitext(file)
+        print ext
+        if ext.lower() == '.tsv':
+            df = pd.read_table(os.path.join('raw_data',file))
+        elif ext.lower() == '.csv':
+            df = pd.read_csv(os.path.join('raw_data',file))
+        else:
+            raise ProcessError('Unknown file type')
+
+        df.to_json(os.path.join('json',base+'.json'))
